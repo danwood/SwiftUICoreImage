e43b407ccb334b55dcbdf3f8acbc7c11dd10423e
Dan Wood <danwood@users.noreply.github.com>
2023-05-17 10:47:07 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,13 +19,14 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
-    exclude: ["Generator"],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftUICoreImage",
             dependencies: [],
-			path: "Sources"),
+			path: "Sources",
+            exclude: ["../Generator"]
+        ),
     ]
 )
