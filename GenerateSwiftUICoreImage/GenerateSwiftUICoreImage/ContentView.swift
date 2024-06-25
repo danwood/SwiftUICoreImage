96b8a50ffe7452b498659b62ba980e97ab9cf9eb
Dan Wood <danwood@users.noreply.github.com>
2024-06-25 15:30:36 -0700

    simple wrapper project to run the generation code

---
+++ GenerateSwiftUICoreImage/GenerateSwiftUICoreImage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GenerateSwiftUICoreImage
+//
+//  Created by Dan Wood on 6/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("See console output")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GenerateSwiftUICoreImage/GenerateSwiftUICoreImage/GenerateSwiftUICoreImageApp.swift
@@ -0,0 +1,18 @@
+//
+//  GenerateSwiftUICoreImageApp.swift
+//  GenerateSwiftUICoreImage
+//
+//  Created by Dan Wood on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct GenerateSwiftUICoreImageApp: App {
+    var body: some Scene {
+        let _ = dumpFilters()
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
